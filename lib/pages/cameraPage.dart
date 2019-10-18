e3c25e9088251f71789882d06eb0cb32a9ea105c
ashishraman96 <ashishraman81@gmail.com>
2019-10-19 02:20:46 +0530

    zuko:Upload long lat

--- lib/pages/cameraPage.dart
+++ lib/pages/cameraPage.dart
@@ -3,7 +3,11 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:location/location.dart';
+
 
 class CameraPage extends StatefulWidget {
   @override
@@ -12,37 +16,75 @@ class CameraPage extends StatefulWidget {
 
 class _CameraPageState extends State<CameraPage> {
   File _image;
-  // or new Dio with a BaseOptions instance.
+  LocationData currentLocation;
+  bool locationFlagError = false;
+
   static BaseOptions options = new BaseOptions(
-  baseUrl: "http://localhost:8000",
+  baseUrl: "http://192.168.8.236:6969",
   connectTimeout: 5000,
   receiveTimeout: 3000,
+
   );
 
   Dio dio = new Dio(options);
 
   Future uploadToServer() async {
-    FormData formData = FormData.fromMap({
-      "name": "wendux",
-      "age": 25,
-      "file": await MultipartFile.fromFile(_image.path,filename: "image.jpg"),
-    });
-    var response = await dio.post("/info", data: formData);
+    FormData formData= new FormData();
+
+    if (currentLocation == null){
+      formData = FormData.fromMap({
+        "lat": "0",
+        "long": "0",
+        "file": await MultipartFile.fromFile(_image.path, filename: "image"),
+      });
+    }else{
+      formData = FormData.fromMap({
+        "lat": currentLocation.latitude.toString(),
+        "long": currentLocation.longitude.toString(),
+        "file": await MultipartFile.fromFile(_image.path, filename: "image"),
+      });
+
+
+    }
+
+    var response = await dio.post("/detect", data: formData);
     log(response.toString());
+  }
+
+  Future getLatLong() async{
+    var location = new Location();
+
+// Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      currentLocation = await location.getLocation();
+    } on PlatformException catch (e) {
+      log(e.toString());
+      if (e.code == 'PERMISSION_DENIED') {
+        var error = 'Permission denied';
+        Fluttertoast.showToast(
+            msg: "Error:"+ error,
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIos: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0
+        );
+      }
+      currentLocation = null;
+    }
   }
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
 
+
     setState(() {
       _image = image;
     });
+    getLatLong();
     uploadToServer();
+
   }
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
