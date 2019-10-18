153383a97ba062ce699273efa493613aee6489ca
ashishraman96 <ashishraman81@gmail.com>
2019-10-18 21:16:43 +0530

    zuko: Upload image to server

--- lib/pages/cameraPage.dart
+++ lib/pages/cameraPage.dart
@@ -1,6 +1,8 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 import 'package:image_picker/image_picker.dart';
 
 class CameraPage extends StatefulWidget {
@@ -10,14 +12,36 @@ class CameraPage extends StatefulWidget {
 
 class _CameraPageState extends State<CameraPage> {
   File _image;
+  // or new Dio with a BaseOptions instance.
+  static BaseOptions options = new BaseOptions(
+  baseUrl: "http://localhost:8000",
+  connectTimeout: 5000,
+  receiveTimeout: 3000,
+  );
 
+  Dio dio = new Dio(options);
+
+  Future uploadToServer() async {
+    FormData formData = FormData.fromMap({
+      "name": "wendux",
+      "age": 25,
+      "file": await MultipartFile.fromFile(_image.path,filename: "image.jpg"),
+    });
+    var response = await dio.post("/info", data: formData);
+    log(response.toString());
+  }
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
 
     setState(() {
       _image = image;
     });
+    uploadToServer();
   }
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
